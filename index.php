40b60fc9a5842b8baafb50efc7037eaff9b6375e
Marcinxxl2 <marcinxxl22@gmail.com>
2017-10-25 21:52:36 +0200

    Początkowa struktura folderów

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HackMe ZSK</title>
+</head>
+<body>
+    
+</body>
+</html>
