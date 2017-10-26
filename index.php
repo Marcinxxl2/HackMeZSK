7c2919098b76338ebcdcf9ad1ad492bdc0c75073
Marcinxxl2 <marcinxxl22@gmail.com>
2017-10-26 20:28:29 +0200

    css

--- index.php
+++ index.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="/HackMeZSK/includes/css/main.css">
     <title>HackMe ZSK</title>
 </head>
 <body>
-    
+    <div id="container">
+        <div id="head">
+            <div id="logoDiv"><a href="/HackMeZSK"><img src="/HackMeZSK/includes/img/logo.png" alt="logo"></a></div>
+            <div class="headButton">Zarejestruj się</div>
+            <div class="headButton">Zaloguj się</div>
+        </div>
+        <div id="leftSide">
+            
+        </div>
+        <div id="content">
+
+        </div>
+        <div id="rightSide">
+        
+        </div>
+        <div id="footer">
+        
+        </div>
+    </div>
 </body>
 </html>
