dbbb80614cebafb5989e9a05855977fd40e47555
Marcinxxl2 <marcinxxl22@gmail.com>
2017-10-28 00:05:59 +0200

    tabelka zrobiona

--- index.php
+++ index.php
@@ -14,13 +14,37 @@
             <div class="headButton">Zaloguj się</div>
         </div>
         <div id="leftSide">
-            
+            <p>Kategorie</p>
+            <div>HTML</div>
+            <div>JavaScript</div>
+            <div>PHP</div>
+            <div>SQL</div>
         </div>
         <div id="content">
 
         </div>
         <div id="rightSide">
-        
+            <!-- Na razie testowa, potem zrobić auto generowanie w php -->
+            <table>
+                <tr>
+                    <th>#</th><th>Nazwa użytkownika</th><th>Punkty</th>
+                </tr>
+                <tr>
+                    <td>1.</td><td>test</td><td>3123</td>
+                </tr>
+                <tr>
+                    <td>2.</td><td>test2</td><td>2131</td>
+                </tr>
+                <tr>
+                    <td>3.</td><td>test3</td><td>1184</td>
+                </tr>
+                <tr>
+                    <td>4.</td><td>test4</td><td>459</td>
+                </tr>
+                <tr>
+                    <td>5.</td><td>test5</td><td>234</td>
+                </tr>
+            </table>
         </div>
         <div id="footer">
         
