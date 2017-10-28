9b186c39e466cf01795ad76c7dbb0ee2cac7dd98
Marcinxxl2 <marcinxxl22@gmail.com>
2017-10-28 18:46:53 +0200

    trochę pozmieniana struktura folderów

---
+++ includes/php/beforeContent.php
@@ -0,0 +1,26 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="/HackMeZSK/includes/css/main.css">
+    <title>HackMe ZSK</title>
+</head>
+<body>
+    <div id="container">
+        <div id="head">
+            <div id="logoDiv"><a href="/HackMeZSK"><img src="/HackMeZSK/includes/img/logo.png" alt="logo"></a></div>
+            <div class="headButton">Zarejestruj się</div>
+            <div class="headButton">Zaloguj się</div>
+        </div>
+        <div id="leftSide">
+            <p>Kategorie</p>
+            <div>HTML</div>
+            <div>JavaScript</div>
+            <div>PHP</div>
+            <div>SQL</div>
+        </div>
+
--- index.php
+++ index.php
@@ -1,54 +1,11 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="/HackMeZSK/includes/css/main.css">
-    <title>HackMe ZSK</title>
-</head>
-<body>
-    <div id="container">
-        <div id="head">
-            <div id="logoDiv"><a href="/HackMeZSK"><img src="/HackMeZSK/includes/img/logo.png" alt="logo"></a></div>
-            <div class="headButton">Zarejestruj się</div>
-            <div class="headButton">Zaloguj się</div>
-        </div>
-        <div id="leftSide">
-            <p>Kategorie</p>
-            <div>HTML</div>
-            <div>JavaScript</div>
-            <div>PHP</div>
-            <div>SQL</div>
-        </div>
-        <div id="content">
+<?php 
+    require $_SERVER['DOCUMENT_ROOT'].'/HackMeZSK/includes/php/beforeContent.php'; 
+?>
 
-        </div>
-        <div id="rightSide">
-            <!-- Na razie testowa, potem zrobić auto generowanie w php -->
-            <table>
-                <tr>
-                    <th>#</th><th>Nazwa użytkownika</th><th>Punkty</th>
-                </tr>
-                <tr>
-                    <td>1.</td><td>test</td><td>3123</td>
-                </tr>
-                <tr>
-                    <td>2.</td><td>test2</td><td>2131</td>
-                </tr>
-                <tr>
-                    <td>3.</td><td>test3</td><td>1184</td>
-                </tr>
-                <tr>
-                    <td>4.</td><td>test4</td><td>459</td>
-                </tr>
-                <tr>
-                    <td>5.</td><td>test5</td><td>234</td>
-                </tr>
-            </table>
-        </div>
-        <div id="footer">
-        
-        </div>
-    </div>
-</body>
-</html>
+<div id="content">
+    
+</div>
+
+<?php 
+    require $_SERVER['DOCUMENT_ROOT'].'/HackMeZSK/includes/php/afterContent.php'; 
+?>
