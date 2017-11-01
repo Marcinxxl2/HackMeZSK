615cf43380be901ab0a2bb77eacd10a05a45d9e0
Marcinxxl2 <marcinxxl22@gmail.com>
2017-11-01 20:21:49 +0100

    formularz rejestracji css

--- registration/index.php
+++ registration/index.php
@@ -3,18 +3,52 @@
     <title>Rejestracja</title>
     <link rel="stylesheet" type="text/css" href="/HackMeZSK/includes/css/regis.css">
     <script src="../includes/js/regis.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 
 <?php /* </HEAD> */ require $_SERVER['DOCUMENT_ROOT'].'/HackMeZSK/includes/php/afterHeadA.php'; ?>
 
 <div id="content">
     <form action="regis.php" method="POST" id="regisForm">
-        <div id="inputLoginDiv">
-            Login:<br>
-            <input type="text" name="login" maxlength="45">
+        <div class="regisSide">
+            <div>
+                <div class="regisLegend">Podaj login: *</div>
+                <input type="text" name="login" maxlength="45">
+                <div id="alertLogin" class="regisAlert"></div>
+            </div>
+            <div>
+                <div class="regisLegend">Podaj imię:</div>
+                <input type="text" name="firstname" maxlength="45">
+                <div id="alertFirstname" class="regisAlert"></div>
+            </div>
+            <div>
+                <div class="regisLegend">Podaj hasło: *</div>
+                <input type="password" name="password1" maxlength="45">
+                <div id="alertPassword1" class="regisAlert"></div>
+            </div>
+            <div id="regisRegulations">
+                <label for="regulationsCheckbox">Zaakceptuj</label> <a href="../includes/docs/regulamin_strony" target="_blank" id="regulationsLink">regulamin</a>: <input type="checkbox" name="regulations" id="regulationsCheckbox">
+                <div id="alertRegulations" class="regisAlert"></div>
+            </div>
+            <div class="g-recaptcha" data-sitekey="6LchbRQUAAAAAKNfB7TwFroRDo4JZa3plUU_pmZG"></div>
+            <button type="button" class="niceButton">Zarejestruj</button>
+            <p>* Pola obowiązkowe</p>
         </div>
-        <div id="inputEmailDiv">
-            Email:<br>
-            <input type="text" name="email" maxlength="45">
+        <div class="regisSide">
+            <div>
+                <div class="regisLegend">Podaj E-mail: *</div>
+                <input type="text" name="email" maxlength="45">
+                <div id="alertEmail" class="regisAlert"></div>
+            </div>
+            <div>
+                <div class="regisLegend">Podaj nazwisko:</div>
+                <input type="text" name="lastname" maxlength="45">
+                <div id="alertLastname" class="regisAlert"></div>
+            </div>
+            <div>
+                <div class="regisLegend">Powtórz hasło: *</div>
+                <input type="password" name="password2" maxlength="45">
+                <div id="alertPassword2" class="regisAlert"></div>
+            </div>
         </div>
     </form>
 </div>
